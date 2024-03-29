ead4116228403fd896a7fb3309d5b36ec0860e11
An_Dev <theesan2k1@gmail.com>
2024-03-29 15:29:51 +0700

    feat: Generate new Db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-
+import { ArticleModule } from './article/article.module';
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ArticleModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
